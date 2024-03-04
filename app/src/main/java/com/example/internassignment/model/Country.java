57e6928985119c006dc060df8a9bd38a3fc59c85
Suneet Murmu <suneet911@gmail.com>
2024-03-05 03:39:56 +0530

    Api Integration

---
+++ app/src/main/java/com/example/internassignment/model/Country.java
@@ -0,0 +1,22 @@
+package com.example.internassignment.model;
+
+public class Country {
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setIso2(String iso2) {
+        this.iso2 = iso2;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getIso2() {
+        return iso2;
+    }
+
+    public String name;
+    public String iso2;
+}
